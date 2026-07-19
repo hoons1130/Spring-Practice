24f4d7529bd9cb8c96f53dd78ba9937ffdc57309
Jae Hoon Choi <hoon11302000@gmail.com>
2026-07-19 22:11:08 +0900

    Todo list

---
+++ todolist/src/main/java/com/example/todolist/domain/Todo.java
@@ -0,0 +1,49 @@
+package com.example.todolist.domain;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Table(name = "todo")
+public class Todo {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private String list;
+
+    @Column(nullable = false)
+    private LocalDate date;
+
+    public Todo(
+            String name,
+            String list,
+            LocalDate date
+    ) {
+        this.name = name;
+        this.list = list;
+        this.date = date;
+    }
+
+    public void update(
+            String name,
+            String list,
+            LocalDate date
+    ) {
+        this.name = name;
+        this.list = list;
+        this.date = date;
+    }
+}
